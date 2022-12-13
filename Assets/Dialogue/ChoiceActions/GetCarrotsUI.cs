cbb1b91937ec632503cc9bdafe54d438389fa33b
fbussabu <filmmy2000@hotmail.com>
2022-12-13 13:43:04 +0100

    carrots ui code

---
+++ Assets/Dialogue/ChoiceActions/GetCarrotsUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GetCarrotsUI : Action
+{
+    public GameObject carrotsUI;
+    public override void DoAction()
+    {
+        carrotsUI.SetActive(true);
+    }
+}
+
