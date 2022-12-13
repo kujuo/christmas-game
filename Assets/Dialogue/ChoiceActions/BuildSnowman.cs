a582e2a2e8e63c5029ff0bbc3cabdbebc2669eeb
fbussabu <filmmy2000@hotmail.com>
2022-12-13 14:45:53 +0100

    Build Snowman UI added

--- Assets/Dialogue/ChoiceActions/BuildSnowman.cs
+++ Assets/Dialogue/ChoiceActions/BuildSnowman.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BuildSnowman : Action
 {
+    public GameObject carrotsUI;
     public override void DoAction()
     {
+        GameObject carrots = Instantiate(carrotsUI);
+        carrots.SetActive(true);
         StoryManager manager = StoryManager.Instance;
         manager.Characters["LITTLE_GIRL"].DialogueCount = 2;
         manager.Characters["SVEN"].DialogueCount = 1;
