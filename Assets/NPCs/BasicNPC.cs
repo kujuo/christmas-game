d4bb8d335331b2519818c0ec4158e8016dde72d4
Julia Hou <juahou@gmail.com>
2022-12-14 23:01:44 +0100

    show icons

--- Assets/NPCs/BasicNPC.cs
+++ Assets/NPCs/BasicNPC.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using QuantumTek.QuantumDialogue;
+using UnityEngine.UI;
 
 public class BasicNPC : NPC
 {
+    public string NPCName;
+
     public override void Interact()
     {
         Player.Instance.Pause = true;
         Time.timeScale = 0.0f;
-        CurrDialogue = Instantiate(DialoguePrefab).GetComponentInChildren<DialogueTracker>();
+        GameObject DialogueUI = Instantiate(DialoguePrefab);
+        CurrDialogue = DialogueUI.GetComponentInChildren<DialogueTracker>();
         CurrDialogue.SetDialogue(Dialogues[DialogueCount]);
+
         CurrDialogue.SetConversation(Conversations[DialogueCount]);
+        QD_Dialogue dialogue = Dialogues[DialogueCount];
+        Sprite icon = dialogue.GetSpeaker(NPCName).Icon;
+        DialogueUI.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<Image>().sprite = icon;
     }
 } 
